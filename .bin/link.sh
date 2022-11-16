7a784d729273451dc71be47de161892f5202cf52
emgniddikur <yoshiharu2580@gmail.com>
2022-11-17 05:10:24 +0900

    add VSCode to link.sh

---
+++ .bin/link.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ROOT_DIR="$(cd ../ && pwd)"
+
+# VSCode
+VSCODE_TARGET_DIR="$ROOT_DIR/.vscode"
+VSCODE_LINK_NAME_DIR="${HOME}/Library/Application Support/Code/User"
+
+for FILE in $(cd "$VSCODE_TARGET_DIR" && ls); do
+  if [ "$FILE" = "extensions" ]; then
+    continue
+  fi
+
+  ln -sfnv "$VSCODE_TARGET_DIR/$FILE" "$VSCODE_LINK_NAME_DIR/$FILE"
+done
