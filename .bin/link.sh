8a522baf0765568d28d1ca0dbcf4072e0d61007e
emgniddikur <yoshiharu2580@gmail.com>
2022-11-18 12:00:31 +0900

    move .vscode/ to vscode/

--- .bin/link.sh
+++ .bin/link.sh
@@ -3,7 +3,7 @@
 ROOT_DIR="$(cd ../ && pwd)"
 
 # VSCode
-VSCODE_TARGET_DIR="$ROOT_DIR/.vscode"
+VSCODE_TARGET_DIR="$ROOT_DIR/vscode"
 VSCODE_LINK_NAME_DIR="${HOME}/Library/Application Support/Code/User"
 
 for FILE in $(cd "$VSCODE_TARGET_DIR" && ls); do
