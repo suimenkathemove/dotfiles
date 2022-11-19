cc263fe99691499c8e47b5e7088e1c7d525ad06a
emgniddikur <yoshiharu2580@gmail.com>
2022-11-20 08:07:25 +0900

    add dev.sh

---
+++ dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PROJECT_NAME="dotfiles"
+
+DEV_DIR="${HOME}/development/emgniddikur"
+
+cd "$DEV_DIR/$PROJECT_NAME"
+
+if tmux has-session -t "$PROJECT_NAME"; then
+  tmux attach-session -t "$PROJECT_NAME"
+else
+  tmux new-session -s "$PROJECT_NAME" -d
+  tmux attach-session -t "$PROJECT_NAME"
+fi
