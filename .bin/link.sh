fe0adb1c1debabf2decdef5f101e8bad6562f8c9
emgniddikur <yoshiharu2580@gmail.com>
2022-11-28 15:14:07 +0900

    use ln_files_in_dir

--- .bin/link.sh
+++ .bin/link.sh
@@ -12,18 +12,10 @@ ln_files_in_dir() {
 ln_files_in_dir "$DOTFILES_PATH/zsh" "$HOME"
 
 # .commands
-COMMANDS_TARGET_DIR="$DOTFILES_PATH/.commands"
 COMMANDS_LINK_NAME_DIR="${HOME}/.commands"
 
 mkdir -p "$COMMANDS_LINK_NAME_DIR"
-for FILE in $(cd "$COMMANDS_TARGET_DIR" && ls); do
-  ln -sfnv "$COMMANDS_TARGET_DIR/$FILE" "$COMMANDS_LINK_NAME_DIR/$FILE"
-done
+ln_files_in_dir "$COMMANDS_LINK_NAME_DIR" "${HOME}/.commands"
 
 # VSCode
-VSCODE_TARGET_DIR="$DOTFILES_PATH/vscode"
-VSCODE_LINK_NAME_DIR="${HOME}/Library/Application Support/Code/User"
-
-for FILE in $(cd "$VSCODE_TARGET_DIR" && ls); do
-  ln -sfnv "$VSCODE_TARGET_DIR/$FILE" "$VSCODE_LINK_NAME_DIR/$FILE"
-done
+ln_files_in_dir "$DOTFILES_PATH/vscode" "${HOME}/Library/Application Support/Code/User"
