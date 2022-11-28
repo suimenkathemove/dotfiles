ea07a5cfc0cd4a7931a859edf60255d1076d8b06
emgniddikur <yoshiharu2580@gmail.com>
2022-11-28 17:32:58 +0900

    fix cd command

--- dev.sh
+++ dev.sh
@@ -4,7 +4,7 @@ PROJECT_NAME="dotfiles"
 
 DEV_DIR="${HOME}/development/suimenkathemove"
 
-cd "$DEV_DIR/$PROJECT_NAME"
+cd "$DEV_DIR/$PROJECT_NAME" || exit
 
 if tmux has-session -t "$PROJECT_NAME"; then
   tmux attach-session -t "$PROJECT_NAME"
