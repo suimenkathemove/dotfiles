82654097c5db366f31249a2de94e309fd7293092
emgniddikur <yoshiharu2580@gmail.com>
2022-11-29 10:54:10 +0900

    add nvim to link.sh

--- .bin/link.sh
+++ .bin/link.sh
@@ -17,5 +17,8 @@ COMMANDS_LINK_NAME_DIR="${HOME}/.commands"
 mkdir -p "$COMMANDS_LINK_NAME_DIR"
 ln_files_in_dir "$COMMANDS_LINK_NAME_DIR" "${HOME}/.commands"
 
+# nvim
+ln -sfnv "$DOTFILES_PATH/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+
 # VSCode
 ln_files_in_dir "$DOTFILES_PATH/vscode" "${HOME}/Library/Application Support/Code/User"
