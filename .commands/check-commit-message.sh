83831f6a9fb22fc96d4e04e7a2a3f355d9d84ff2
suimenkathemove <yoshiharu2580@gmail.com>
2024-01-27 00:05:53 +0900

    fix check-commit-message.sh

--- .commands/check-commit-message.sh
+++ .commands/check-commit-message.sh
@@ -6,6 +6,9 @@ last_commit_message=$(git log -1 --pretty=%B)
 
 if [ "$expected" = "$last_commit_message" ]; then
   echo o
+
+  current_branch_name=$(git branch | grep \* | cut -d ' ' -f2)
+  git push origin "$current_branch_name"
 else
   echo x
 fi
