b708011402035e97af956894f241f073d093fabf
suimenkathemove <yoshiharu2580@gmail.com>
2024-02-24 09:11:42 +0900

    fix gitccm

--- .commands/check-commit-message.sh
+++ .commands/check-commit-message.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+current_branch=$(git branch | grep \* | cut -d ' ' -f2)
+
+commit_count=$(git rev-list --count origin/$current_branch..$current_branch)
+echo commit_count is $commit_count
+
+if [[ $commit_count -ne 1 ]]; then
+  exit 1
+fi
+
 expected="$1"
 
 last_commit_message=$(git log -1 --pretty=%B)
@@ -7,8 +16,7 @@ last_commit_message=$(git log -1 --pretty=%B)
 if [ "$expected" = "$last_commit_message" ]; then
   echo o
 
-  current_branch_name=$(git branch | grep \* | cut -d ' ' -f2)
-  git push origin "$current_branch_name"
+  git push origin "$current_branch"
 
   git branch -D trial
 else
