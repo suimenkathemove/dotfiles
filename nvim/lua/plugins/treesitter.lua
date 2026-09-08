1fa38b8d783231e9186f31e89e1070431f7a844a
kato-yoshiharu <yoshiharu2580@gmail.com>
2026-09-09 02:48:18 +0900

    confirmed

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -59,6 +59,7 @@ return {
       pattern = filetypes,
       callback = function()
         pcall(vim.treesitter.start)
+        -- インデントは experimental 扱い。問題が出たらこの行を消す
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
 }
